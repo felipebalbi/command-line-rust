e8966884a9399dbd5f57455cf6f3293f2480635c
Felipe Balbi <felipe@balbi.sh>
2022-08-01 22:53:25 +0300

    chapter 3 done
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

---
+++ catr/src/lib.rs
@@ -0,0 +1,90 @@
+use clap::{App, Arg};
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("catr")
+        .version("0.1.0")
+        .author("Felipe Balbi <felipe@balbi.sh")
+        .about("Rust cat")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("number")
+                .short("n")
+                .long("number")
+                .help("Number lines")
+                .takes_value(false)
+                .conflicts_with("number_nonblank"),
+        )
+        .arg(
+            Arg::with_name("number_nonblank")
+                .short("b")
+                .long("number-nonblank")
+                .help("Number non-blank lines")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    let files = matches.values_of_lossy("files").unwrap();
+    let number_lines = matches.is_present("number");
+    let number_nonblank_lines = matches.is_present("number_nonblank");
+
+    Ok(Config {
+        files,
+        number_lines,
+        number_nonblank_lines,
+    })
+}
+
+pub fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    for filename in config.files {
+        match open(&filename) {
+            Err(e) => eprintln!("Failed to open {}: {}", filename, e),
+            Ok(reader) => {
+                let mut last_num = 0;
+
+                for (num, line) in reader.lines().enumerate() {
+                    let line = line?;
+
+                    if config.number_lines {
+                        println!("{:>6}\t{}", num + 1, line);
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:>6}\t{}", last_num, line);
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
