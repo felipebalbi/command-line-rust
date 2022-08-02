60653faac47de5111f379a672141e60516b0f7e2
Felipe Balbi <felipe@balbi.sh>
2022-08-02 21:23:21 +0300

    chapter 4: Completed
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -65,8 +67,47 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:#?}", config);
+    let num_files = config.files.len();
+
+    for (num, filename) in config.files.iter().enumerate() {
+        match open(&filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(mut reader) => {
+                if num_files > 1 {
+                    println!("{}==> {} <==", if num > 0 { "\n" } else { "" }, &filename);
+                }
+
+                if let Some(bytes) = config.bytes {
+                    let mut buffer = vec![0; bytes];
+                    let bytes_read = reader.read(&mut buffer)?;
+
+                    print!("{}", String::from_utf8_lossy(&buffer[..bytes_read]));
+                } else {
+                    let mut line = String::new();
+
+                    for _ in 0..config.lines {
+                        let bytes = reader.read_line(&mut line)?;
+
+                        if bytes == 0 {
+                            break;
+                        }
+
+                        print!("{}", line);
+                        line.clear();
+                    }
+                }
+            }
+        }
+    }
+
     Ok(())
+}
+
+pub fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
 
 fn parse_positive_int(val: &str) -> MyResult<usize> {
