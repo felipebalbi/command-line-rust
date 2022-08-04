a907e99fd9b5ff1c8e10dd8ac855089ed9ada2cc
Felipe Balbi <felipe@balbi.sh>
2022-08-04 19:48:40 +0300

    uniqr: add basic command line argument parsing
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

---
+++ uniqr/src/lib.rs
@@ -0,0 +1,54 @@
+use clap::{App, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    in_file: String,
+    out_file: Option<String>,
+    count: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("uniqr")
+        .version("0.1.0")
+        .author("Felipe Balbi <felipe@balbi.sh>")
+        .about("Rust uniq")
+        .arg(
+            Arg::with_name("in_file")
+                .value_name("IN_FILE")
+                .help("Input file")
+                .takes_value(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("out_file")
+                .value_name("OUT_FILE")
+                .help("Output file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("count")
+                .short("c")
+                .long("count")
+                .help("Prefix lines by the number of occurrences"),
+        )
+        .get_matches();
+
+    let in_file = matches.value_of("in_file").unwrap().to_string();
+    let out_file = matches.value_of("out_file").map(String::from);
+    let count = matches.is_present("count");
+
+    Ok(Config {
+        in_file,
+        out_file,
+        count,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:?}", config);
+
+    Ok(())
+}
--- uniqr/src/main.rs
+++ uniqr/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = uniqr::get_args().and_then(uniqr::run) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
 }
