8901c3e71911170161174a796a8a3b62bf4913e3
Felipe Balbi <felipe@balbi.sh>
2022-08-06 19:23:04 +0300

    cutr: passing 10 tests
    Man, this one is pretty hard! I'll have to get back to it in order to
    fully understand it.
    
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

---
+++ cutr/src/lib.rs
@@ -0,0 +1,268 @@
+use crate::Extract::*;
+use clap::{App, Arg};
+use regex::Regex;
+use std::error::Error;
+use std::num::NonZeroUsize;
+use std::ops::Range;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+type PositionList = Vec<Range<usize>>;
+
+#[derive(Debug)]
+pub enum Extract {
+    Fields(PositionList),
+    Bytes(PositionList),
+    Chars(PositionList),
+}
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    extract: Extract,
+    delimiter: u8,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("cutr")
+        .version("0.1.0")
+        .author("Felipe Balbi <felipe@balbi.sh>")
+        .about("Rust cut")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .default_value("-")
+                .multiple(true),
+        )
+        .arg(
+            Arg::with_name("delim")
+                .value_name("DELIMITER")
+                .help("Field delimiter")
+                .short("d")
+                .long("delim")
+                .takes_value(true)
+                .default_value("\t"),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .value_name("BYTES")
+                .help("Selected bytes")
+                .short("b")
+                .long("bytes")
+                .conflicts_with_all(&["chars", "fields"])
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("chars")
+                .value_name("CHARS")
+                .help("Selected characters")
+                .short("c")
+                .long("chars")
+                .conflicts_with_all(&["bytes", "fields"])
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("fields")
+                .value_name("FIELDS")
+                .help("Selected fields")
+                .short("f")
+                .long("fields")
+                .conflicts_with_all(&["bytes", "chars"])
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let files = matches.values_of_lossy("files").unwrap();
+
+    let delimiter = matches.value_of("delim").unwrap();
+    let delimiter_bytes = delimiter.as_bytes();
+
+    if delimiter_bytes.len() != 1 {
+        return Err(From::from(format!(
+            "--delim \"{}\" must be a single byte",
+            delimiter
+        )));
+    }
+
+    let delimiter = *delimiter_bytes.first().unwrap();
+
+    let bytes = matches.value_of("bytes").map(parse_pos).transpose()?;
+    let chars = matches.value_of("chars").map(parse_pos).transpose()?;
+    let fields = matches.value_of("fields").map(parse_pos).transpose()?;
+
+    let extract = if let Some(byte_pos) = bytes {
+        Bytes(byte_pos)
+    } else if let Some(char_pos) = chars {
+        Chars(char_pos)
+    } else if let Some(field_pos) = fields {
+        Fields(field_pos)
+    } else {
+        return Err(From::from("Must have --fields, --bytes, or --chars"));
+    };
+
+    Ok(Config {
+        files,
+        extract,
+        delimiter,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", &config);
+
+    Ok(())
+}
+
+fn parse_pos(range: &str) -> MyResult<PositionList> {
+    let re = Regex::new(r"^([0-9]+)-([0-9]+)$").unwrap();
+
+    range
+        .split(',')
+        .into_iter()
+        .map(|val| {
+            parse_index(val).map(|n| n..n + 1).or_else(|e| {
+                re.captures(val).ok_or(e).and_then(|captures| {
+                    let n1 = parse_index(&captures[1])?;
+                    let n2 = parse_index(&captures[2])?;
+
+                    if n1 >= n2 {
+                        return Err(format!(
+                            "First number in range ({}) must be lower than second number ({})",
+                            n1 + 1,
+                            n2 + 1
+                        ));
+                    }
+                    Ok(n1..n2 + 1)
+                })
+            })
+        })
+        .collect::<Result<_, _>>()
+        .map_err(From::from)
+}
+
+fn parse_index(index: &str) -> Result<usize, String> {
+    let err = || format!("illegal list value: \"{}\"", index);
+
+    index
+        .starts_with('+')
+        .then(|| Err(err()))
+        .unwrap_or_else(|| {
+            index
+                .parse::<NonZeroUsize>()
+                .map(|n| usize::from(n) - 1)
+                .map_err(|_| err())
+        })
+}
+
+#[cfg(test)]
+mod unit_tests {
+    use super::parse_pos;
+
+    #[test]
+    fn test_parse_pos() {
+        // The empty string is an error
+        assert!(parse_pos("").is_err());
+
+        // Zero is an error
+        let res = parse_pos("0");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"0\"",);
+
+        let res = parse_pos("0-1");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"0\"",);
+
+        // A leading "+" is an error
+        let res = parse_pos("+1");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"+1\"",);
+
+        let res = parse_pos("+1-2");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"+1-2\"",);
+
+        // Any non-number is an error
+        let res = parse_pos("a");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"a\"",);
+
+        let res = parse_pos("1,a");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"a\"",);
+
+        let res = parse_pos("1-a");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"1-a\"",);
+
+        let res = parse_pos("a-1");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "illegal list value: \"a-1\"",);
+
+        // Wonky ranges
+        let res = parse_pos("-");
+        assert!(res.is_err());
+
+        let res = parse_pos(",");
+        assert!(res.is_err());
+
+        let res = parse_pos("1,");
+        assert!(res.is_err());
+
+        let res = parse_pos("1-");
+        assert!(res.is_err());
+
+        let res = parse_pos("1-1-1");
+        assert!(res.is_err());
+
+        let res = parse_pos("1-1-a");
+        assert!(res.is_err());
+
+        // First number must be less than second
+        let res = parse_pos("1-1");
+        assert!(res.is_err());
+        assert_eq!(
+            res.unwrap_err().to_string(),
+            "First number in range (1) must be lower than second number (1)"
+        );
+
+        let res = parse_pos("2-1");
+        assert!(res.is_err());
+        assert_eq!(
+            res.unwrap_err().to_string(),
+            "First number in range (2) must be lower than second number (1)"
+        );
+
+        // All the following are acceptable
+        let res = parse_pos("1");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), vec![0..1]);
+
+        let res = parse_pos("01");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), vec![0..1]);
+
+        let res = parse_pos("1,3");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), vec![0..1, 2..3]);
+
+        let res = parse_pos("001,0003");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), vec![0..1, 2..3]);
+
+        let res = parse_pos("1-3");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), vec![0..3]);
+
+        let res = parse_pos("0001-03");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), vec![0..3]);
+
+        let res = parse_pos("1,7,3-5");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), vec![0..1, 6..7, 2..5]);
+
+        let res = parse_pos("15,19-20");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), vec![14..15, 18..20]);
+    }
+}
--- cutr/src/main.rs
+++ cutr/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = cutr::get_args().and_then(cutr::run) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
 }
