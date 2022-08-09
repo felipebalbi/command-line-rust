e491ba73c4c3154f94721317596e0bf5976131fd
Felipe Balbi <felipe@balbi.sh>
2022-08-09 11:02:30 +0300

    grepr: passing 3 tests
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- grepr/src/lib.rs
+++ grepr/src/lib.rs
@@ -1,6 +1,8 @@
 use clap::{App, Arg};
 use regex::{Regex, RegexBuilder};
 use std::error::Error;
+use std::fs::{self, File};
+use walkdir::WalkDir;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -82,7 +84,99 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:#?}", config);
+    println!("pattern \"{}\"", config.pattern);
+
+    let entries = find_files(&config.files, config.recursive);
+
+    for entry in entries {
+        match entry {
+            Err(e) => eprintln!("{}", e),
+            Ok(filename) => println!("file \"{}\"", filename),
+        }
+    }
 
     Ok(())
+}
+
+fn find_files(paths: &[String], recursive: bool) -> Vec<MyResult<String>> {
+    let mut files = vec![];
+
+    for path in paths {
+        match path.as_str() {
+            "-" => files.push(Ok(path.to_string())),
+            _ => match fs::metadata(path) {
+                Ok(metadata) => {
+                    if metadata.is_dir() {
+                        if !recursive {
+                            files.push(Err(From::from(format!("{} is a directory", path))));
+                        } else {
+                            for entry in WalkDir::new(path)
+                                .into_iter()
+                                .flatten()
+                                .filter(|e| e.file_type().is_file())
+                            {
+                                files.push(Ok(entry.path().display().to_string()));
+                            }
+                        }
+                    } else if metadata.is_file() {
+                        files.push(Ok(path.to_string()));
+                    }
+                }
+                Err(e) => files.push(Err(From::from(format!("{}: {}", path, e)))),
+            },
+        }
+    }
+
+    files
+}
+
+#[cfg(test)]
+mod tests {
+    use super::find_files;
+    use rand::{distributions::Alphanumeric, Rng};
+
+    #[test]
+    fn test_find_files() {
+        // Verify that the function finds a file known to exist
+        let files = find_files(&["./tests/inputs/fox.txt".to_string()], false);
+        assert_eq!(files.len(), 1);
+        assert_eq!(files[0].as_ref().unwrap(), "./tests/inputs/fox.txt");
+
+        // The function should reject a directory without the recursive option
+        let files = find_files(&["./tests/inputs".to_string()], false);
+        assert_eq!(files.len(), 1);
+        if let Err(e) = &files[0] {
+            assert_eq!(e.to_string(), "./tests/inputs is a directory");
+        }
+
+        // Verify the function recurses to find four files in the directory
+        let res = find_files(&["./tests/inputs".to_string()], true);
+        let mut files: Vec<String> = res
+            .iter()
+            .map(|r| r.as_ref().unwrap().replace("\\", "/"))
+            .collect();
+        files.sort();
+        assert_eq!(files.len(), 4);
+        assert_eq!(
+            files,
+            vec![
+                "./tests/inputs/bustle.txt",
+                "./tests/inputs/empty.txt",
+                "./tests/inputs/fox.txt",
+                "./tests/inputs/nobody.txt",
+            ]
+        );
+
+        // Generate a random string to represent a nonexistent file
+        let bad: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect();
+
+        // Verify that the function returns the bad file as an error
+        let files = find_files(&[bad], false);
+        assert_eq!(files.len(), 1);
+        assert!(files[0].is_err());
+    }
 }
