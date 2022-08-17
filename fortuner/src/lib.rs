8917843a5f54be2dd6f4fa9261ae2f37877b826a
Felipe Balbi <felipe@balbi.sh>
2022-08-17 22:06:24 +0300

    fortuner: passing 2 tests
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- fortuner/src/lib.rs
+++ fortuner/src/lib.rs
@@ -1,6 +1,10 @@
 use clap::{App, Arg};
 use regex::{Regex, RegexBuilder};
 use std::error::Error;
+use std::ffi::OsStr;
+use std::fs;
+use std::path::PathBuf;
+use walkdir::{DirEntry, WalkDir};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -69,7 +73,8 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:#?}", config);
+    let files = find_files(&config.sources)?;
+    println!("{:#?}", files);
 
     Ok(())
 }
@@ -79,9 +84,34 @@ fn parse_u64(val: &str) -> MyResult<u64> {
         .map_err(|_| format!("\"{}\" not a valid integer", val).into())
 }
 
+fn find_files(paths: &[String]) -> MyResult<Vec<PathBuf>> {
+    let mut files = Vec::new();
+
+    let file_filter = |entry: &DirEntry| {
+        entry.file_type().is_file() && entry.path().extension() != Some(OsStr::new("dat"))
+    };
+
+    for path in paths {
+        match fs::metadata(path) {
+            Err(e) => return Err(format!("{}: {}", path, e).into()),
+            Ok(_) => files.extend(
+                WalkDir::new(path)
+                    .into_iter()
+                    .filter_map(Result::ok)
+                    .filter(file_filter)
+                    .map(|entry| entry.path().into()),
+            ),
+        }
+    }
+
+    files.sort();
+    files.dedup();
+    Ok(files)
+}
+
 #[cfg(test)]
 mod tests {
-    use super::parse_u64;
+    use super::{find_files, parse_u64};
     use std::path::PathBuf;
 
     #[test]
@@ -97,5 +127,53 @@ mod tests {
         let res = parse_u64("4");
         assert!(res.is_ok());
         assert_eq!(res.unwrap(), 4);
+    }
+
+    #[test]
+    fn test_find_files() {
+        // Verify that the function finds a file known to exist
+        let res = find_files(&["./tests/inputs/jokes".to_string()]);
+        assert!(res.is_ok());
+
+        let files = res.unwrap();
+        assert_eq!(files.len(), 1);
+        assert_eq!(
+            files.get(0).unwrap().to_string_lossy(),
+            "./tests/inputs/jokes"
+        );
+
+        // Fails to find a bad file
+        let res = find_files(&["/path/does/not/exist".to_string()]);
+        assert!(res.is_err());
+
+        // Finds all the input files, excludes ".dat"
+        let res = find_files(&["./tests/inputs".to_string()]);
+        assert!(res.is_ok());
+
+        // Check number and order of files
+        let files = res.unwrap();
+        assert_eq!(files.len(), 5);
+        let first = files.get(0).unwrap().display().to_string();
+        assert!(first.contains("ascii-art"));
+        let last = files.last().unwrap().display().to_string();
+        assert!(last.contains("quotes"));
+
+        // Test for multiple sources, path must be unique and sorted
+        let res = find_files(&[
+            "./tests/inputs/jokes".to_string(),
+            "./tests/inputs/ascii-art".to_string(),
+            "./tests/inputs/jokes".to_string(),
+        ]);
+        assert!(res.is_ok());
+
+        let files = res.unwrap();
+        assert_eq!(files.len(), 2);
+        if let Some(filename) = files.first().unwrap().file_name() {
+            assert_eq!(filename.to_string_lossy(), "ascii-art".to_string())
+        }
+
+        if let Some(filename) = files.last().unwrap().file_name() {
+            assert_eq!(filename.to_string_lossy(), "jokes".to_string())
+        }
     }
 }
